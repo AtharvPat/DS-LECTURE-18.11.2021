b5f98358d081a42d8c5f5cfd5b47dd3209075e0a
AtharvPat <93934533+AtharvPat@users.noreply.github.com>
2021-11-18 10:55:08 +0530

    Update BINARY SEARCH.cpp

--- BINARY SEARCH.cpp
+++ BINARY SEARCH.cpp
@@ -4,53 +4,54 @@
 #include <iomanip>
 
 using namespace std;
-void bubbleSort(int *input, int size)
+int binarySearch(int *input, int n, int val)
 {
-	int end = size;
+	int start = 0;
+	int end = n-1;
+	int a = -1;
 
-	for (int i = 0; i < size - 2; i++)
+	for (int i = 0; i < n; i++)
 	{
-		for (int j = 1, a = 0; j < size; j++, a++)
+		int mid = (start + end) / 2;
+		if (input[mid] == val)
 		{
-			if (input[a] > input[j])
-			{
-				int temp = input[a];
-				input[a] = input[j];
-				input[j] = temp;
-			}
-			
+			return mid;
+			break;
 		}
-		end--;
+		else if (input[mid] < val)
+		{
+			start = mid + 1;
+		}
+		else
+		{
+			end = mid - 1;
+		}
 	}
+	return a;
 }
 int main()
 {
-cout<<"enter the number of arrays:-"<<endl;
+
+	int size;
+	cin >> size;
+	int *input = new int[size];
+    cout<<"inputs of array"endl;
+	for(int i = 0; i < size; ++i)
+	{
+		cin >> input[i];
+	}
+      cout<<"enter number of test cases to be run"endl;
 	int t;
 	cin >> t;
 
 	while (t--)
 	{
-        cout<<"enter the size of the array:-"<<endl;
-		int size;
-		cin >> size;
-
-		int *input = new int[size];
-        cout<<"enter the values of the array:-"<<endl;
-
-		for (int i = 0; i < size; ++i)
-		{
-			cin >> input[i];
-		}
-
-		bubbleSort(input, size);
-cout<<"sorted array:-"<<endl;
-		for (int i = 0; i < size; ++i)
-		{
-			cout << input[i] << " ";
-		}
-
-		delete[] input;
-		cout << endl;
+        cout<<"enter the value to be search"endl;
+		int val;
+		cin >> val;
+		cout << binarySearch(input, size, val) << endl;
 	}
+	
+	delete [] input;
+	return 0;
 }
